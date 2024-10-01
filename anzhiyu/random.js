2797e8b8d29786860e3fa94dbc5db8d616f256be
xiaoyan-io <network.io.biz@gmail.com>
2024-10-01 23:55:03 +0630

    Site updated: 2024-10-01 23:55:03

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/15/hello-world/"];function toRandomPost(){
+var posts=["2024/10/01/clash规则配置文件-1/","2024/10/01/openwrt/","2024/10/01/clash规则配置文件/","2024/09/15/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
