0d52aca33772d92d237ec0a8e376e53e007d2ad5
xiaoyan-io <nainghtwe.maung@gmail.com>
2024-10-22 22:48:31 +0630

    Site updated: 2024-10-22 22:48:31

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/20/hello-world/"];function toRandomPost(){
+var posts=["2024/10/22/NewPost/","2024/10/20/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
