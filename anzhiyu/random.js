200d0e5cb853b5069aa220702bbf1b3c049223e7
xiaoyan-io <nainghtwe.maung@gmail.com>
2024-10-23 12:54:18 +0630

    Site updated: 2024-10-23 12:54:18

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/22/NewPost/","2024/10/20/hello-world/"];function toRandomPost(){
+var posts=["2024/10/23/群晖使用docker部署joplin/","2024/10/23/G-Box-安装使用说明/","2024/10/22/新的文章/","2024/10/20/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
