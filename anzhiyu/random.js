8ec3e65dfaa27f9bcb48cdabe02c0d9da219c843
xiaoyan-io <nainghtwe.maung@gmail.com>
2024-10-24 22:56:43 +0630

    Site updated: 2024-10-24 22:56:43

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/24/1Panel-部署-爱影CMS/","2024/10/23/将本地数据备份到远程NAS服务器/","2024/10/23/群晖使用docker部署joplin/","2024/10/23/G-Box-安装使用说明/","2024/10/22/新的文章/","2024/10/20/hello-world/"];function toRandomPost(){
+var posts=["2024/10/24/Ubuntu安装爱影CMS/","2024/10/24/1Panel-部署-爱影CMS/","2024/10/23/将本地数据备份到远程NAS服务器/","2024/10/23/群晖使用docker部署joplin/","2024/10/23/G-Box-安装使用说明/","2024/10/22/新的文章/","2024/10/20/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
